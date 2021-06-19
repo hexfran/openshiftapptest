be09e3781a646916fad86a0f0d1c839a95c39064
Francesco <francesco@hexbyte.net>
2021-06-19 12:16:22 +0200

    Init

---
+++ src/main/java/net/hexbyte/openshift/OpenshiftapptestApplication.java
@@ -0,0 +1,15 @@
+package net.hexbyte.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftapptestApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OpenshiftapptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/hexbyte/openshift/OpenshiftapptestApplicationTests.java
@@ -0,0 +1,15 @@
+package net.hexbyte.openshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftapptestApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
